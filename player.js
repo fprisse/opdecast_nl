b9efbc9ce530d1a74bfca951db1e9ec897e3031f
fprisse <florent@prisse.com>
2021-10-01 14:09:41 +0200

    Radio1

--- player.js
+++ player.js
@@ -13,25 +13,25 @@ songs = ['http://icecast.radiofrance.fr/fip-midfi.mp3',
 'http://icecast.radiofrance.fr/fipjazz-midfi.mp3',
 'http://icecast.radiofrance.fr/fipgroove-midfi.mp3',
 'http://classicalking.streamguys1.com/king-fm-mp3',
-'http://classicalking.streamguys1.com/evergreen-mp3-96k']; // object storing paths for audio objects
+'http://icecast.omroep.nl/radio1-bb-mp3']; // object storing paths for audio objects
 thumbnails = ['./assets/images/fip_radio.png',
 './assets/images/fip_rock.png',
 './assets/images/fip_jazz.png',
 './assets/images/fip_groove.png',
 './assets/images/king1.png',
-'./assets/images/king2.png']; // object storing paths for album covers and backgrounds
+'./assets/images/radio1.png']; // object storing paths for album covers and backgrounds
 songArtists = ['FIP Stream 1',
 'FIP Stream 2',
 'FIP Stream 3',
 'FIP Stream 4',
 'King Seatle',
-'King Seatle']; // object storing track artists
+'NPO']; // object storing track artists
 songTitles = ['Radio',
 'Rock',
 'Jazz',
 'Groove',
 'Classic FM 98.1',
-'Evergreen']; // object storing track titles
+'Radio1']; // object storing track titles
 
 // function where pp (play-pause) element changes based on playing boolean value - if play button clicked, change pp.src to pause button and call song.play() and vice versa.
 let playing = true;
